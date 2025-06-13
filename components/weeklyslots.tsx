6a8f08ef441dd93f61a3d5c7b94e9243a2ff96f6
Riza Anas <nameisriza123boy@gmail.com>
2025-06-13 20:28:36 +0530

    Fix formatting and spacing issues in WeeklySlots component

--- components/weeklyslots.tsx
+++ components/weeklyslots.tsx
@@ -1,10 +1,11 @@
-import CommonHeader from "./common/CommonHeader"
+import CommonHeader from "./common/CommonHeader";
 import ScheduleTable, { ScheduleDay, TimeSlot } from "./common/ScheduleTable";
+import { Button } from "./ui/button";
 
 const days: ScheduleDay[] = [
   { number: "", day: "Mon" },
   { number: "", day: "Tue" },
-  { number: "", day: "Wed"},
+  { number: "", day: "Wed" },
   { number: "", day: "Thu" },
   { number: "", day: "Fri" },
   { number: "", day: "Sat" },
@@ -16,12 +17,13 @@ const timeSlots: TimeSlot[] = Array(10)
   .map(() => ({
     startTime: "10.30 AM",
     endTime: "11 AM",
-    prices: [3000, 3000, 3000, 3000, 3000, 3000,3000],
+    prices: [3000, 3000, 3000, 3000, 3000, 3000, 3000],
   }));
 
 export const WeeklySlots = () => {
-  return (<>
-    <div className="flex flex-col">
+  return (
+    <>
+      <div className="flex flex-col">
         <CommonHeader
           title="Manage Weekly Rate & Availability"
           subtitle="Configure your regular weekly availability and set different rates for weekdays and weekends."
@@ -31,9 +33,15 @@ export const WeeklySlots = () => {
           <ScheduleTable days={days} timeSlots={timeSlots} />
         </div>
 
-
-
-    </div>
-  </>
-  )
-}
+        <div className="flex justify-end">
+          <Button
+            className="w-[150px] h-[44px] bg-green-500 text-white mr-[150px]"
+            variant="outline"
+          >
+            Confirm
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
