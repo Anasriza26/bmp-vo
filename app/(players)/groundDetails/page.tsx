17022505cc400886f3def6db5715810f045b9cdb
Riza Anas <nameisriza123boy@gmail.com>
2025-06-19 13:09:34 +0530

    Add GroundDetails component to player details page

---
+++ app/(players)/groundDetails/page.tsx
@@ -0,0 +1,11 @@
+import GroundDetails from '@/components/players/GroundDetails'
+import React from 'react'
+
+const page = () => {
+  return (<>
+  <GroundDetails />
+  </>
+  )
+}
+
+export default page
