fe936b52b086847719386b8f26dd5fb4c76f42cc
Riza Anas <nameisriza123boy@gmail.com>
2025-06-20 16:08:27 +0530

    Add PaymentSuccess page: create new page component to render PaymentSuccess UI

---
+++ app/(players)/paymentSuccess/page.tsx
@@ -0,0 +1,11 @@
+import PaymentSuccess from '@/components/players/PaymentSuccess'
+import React from 'react'
+
+const page = () => {
+  return (<>
+  <PaymentSuccess />
+  </>
+  )
+}
+
+export default page
