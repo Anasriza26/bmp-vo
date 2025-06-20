e7b04f935e6c46ca3a9a0763ac46e81264f622c6
Riza Anas <nameisriza123boy@gmail.com>
2025-06-20 10:38:46 +0530

    Add OtpVerification component to OTP page

---
+++ app/(players)/otp/page.tsx
@@ -0,0 +1,11 @@
+import OtpVerification from '@/components/players/OtpVerification'
+import React from 'react'
+
+const page = () => {
+  return (<>
+  <OtpVerification />
+  </>
+  )
+}
+
+export default page
