47bfd2ef7a2496b1dbf6f8cc5ae52f1a89d5fe99
Riza Anas <nameisriza123boy@gmail.com>
2025-06-20 15:47:57 +0530

    Add CheckBooking page: create new page component to render CheckBooking UI

---
+++ app/(players)/checkBooking/page.tsx
@@ -0,0 +1,11 @@
+import CheckBooking from '@/components/players/CheckBooking'
+import React from 'react'
+
+const page = () => {
+  return (<>
+  <CheckBooking />
+  </>
+  )
+}
+
+export default page
