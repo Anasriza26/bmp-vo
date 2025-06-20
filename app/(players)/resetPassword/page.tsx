39d5934faa9ebd6d4715ff47ec2e63aa13a706a3
Riza Anas <nameisriza123boy@gmail.com>
2025-06-20 11:17:57 +0530

    Add ResetPassword page: create new page component to render ResetPassword UI

---
+++ app/(players)/resetPassword/page.tsx
@@ -0,0 +1,11 @@
+import ResetPassword from '@/components/players/ResetPassword'
+import React from 'react'
+
+const page = () => {
+  return (<>
+  <ResetPassword />
+  </>
+  )
+}
+
+export default page
