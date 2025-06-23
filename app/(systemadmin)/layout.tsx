984f87bfdfd655d080009b2be47b1550d8ca736a
Riza Anas <nameisriza123boy@gmail.com>
2025-06-23 11:21:00 +0530

    Refactor DashboardLayout: integrate SidebarProvider and AppSidebar for improved layout structure

--- app/(systemadmin)/layout.tsx
+++ app/(systemadmin)/layout.tsx
@@ -1,5 +1,22 @@
-import SidebarLayoutClient from '@/components/layouts/SidebarLayoutClient';
+import AppSidebar from "@/components/common/Sidebar";
+import SidebarLayoutClient from "@/components/layouts/SidebarLayoutClient";
+import {
+  Sidebar,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <SidebarLayoutClient>{children}</SidebarLayoutClient>;
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      {/* <SidebarInset> */}
+        <main className="flex flex-col w-full">{children}</main>
+      {/* </SidebarInset> */}
+    </SidebarProvider>
+  );
 }
