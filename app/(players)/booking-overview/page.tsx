db99396e4fe13e1e749c20924bc309cb3c8541ed
Riza Anas <nameisriza123boy@gmail.com>
2025-06-23 16:52:53 +0530

    Add BookingOverview page: create initial layout for booking overview view

---
+++ app/(players)/booking-overview/page.tsx
@@ -0,0 +1,11 @@
+import BookingOverview from '@/components/players/BookingOverview'
+import React from 'react'
+
+const page = () => {
+  return (<>
+  <BookingOverview />
+  </>
+  )
+}
+
+export default page
