0304b9e9c556048cae7e8835b6e29e5dfd2f95ab
Riza Anas <nameisriza123boy@gmail.com>
2025-06-24 11:57:23 +0530

    Add PaymentDetails page component for onboarding process

---
+++ app/(onboarding)/payment-details/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Paymentdetails from '@/components/onboading/PaymentDetails'
+
+const page = () => {
+  return (<>
+  <Paymentdetails/>
+  </>
+  )
+}
+
+export default page
