53cde4a315918d70154a404833516ecb78038c3b
Riza Anas <nameisriza123boy@gmail.com>
2025-06-24 11:26:30 +0530

    Add Complete component for onboarding completion page

---
+++ app/(onboarding)/complete/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Complete from '@/components/onboading/Complete'
+
+const page = () => {
+  return (<>
+  <main>
+    <Complete/>
+  </main>
+  </>
+  )
+}
+
+export default page
