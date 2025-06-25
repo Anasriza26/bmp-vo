6deb4bb26cb5f8bee7a72b2469c5e94772e3a144
Riza Anas <nameisriza123boy@gmail.com>
2025-06-25 11:25:21 +0530

    Refactor Description component: update heading size for improved readability

--- components/groundDetails/Description.tsx
+++ components/groundDetails/Description.tsx
@@ -1,6 +1,6 @@
 const Description = () => (
   <div>
-    <h3 className="text-xl font-semibold text-gray-900 mb-4">Description</h3>
+    <h3 className="text-lg font-semibold text-gray-900 mb-2">Description</h3>
     <p className="text-gray-600 leading-relaxed">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, suscipit consequatur id assumenda, tenetur commodi asperiores error quas quos dolor libero sint nihil?
     </p>
