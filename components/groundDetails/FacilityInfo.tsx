29b0bcd8c1948fd23816b92e2dc11bf7351471c6
Riza Anas <nameisriza123boy@gmail.com>
2025-06-25 10:23:34 +0530

    Refactor FacilityInfo component: improve layout structure and adjust heading size for better readability

--- components/groundDetails/FacilityInfo.tsx
+++ components/groundDetails/FacilityInfo.tsx
@@ -1,23 +1,34 @@
 import { MapPin, Star } from "lucide-react";
 
-const FacilityInfo = ({name,location,rating} : {name:string,location:string,rating:number}) => (
+const FacilityInfo = ({
+  name,
+  location,
+  rating,
+}: {
+  name: string;
+  location: string;
+  rating: number;
+}) => (
   <div className="flex items-start justify-between mb-4">
     <div>
-      <h1 className="text-3xl font-semibold text-green-600 mb-2">{name}</h1>
-      <div className="flex items-center text-gray-600 mb-3">
-        <MapPin className="h-5 w-5 mr-2" />
-        <span>{location}</span>
-      </div>
-      <div className="flex items-center">
-        <div className="flex items-center mr-3">
-          {[...Array(5)].map((_, i) => (
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-semibold text-green-600 mb-2">{name}</h1>
+        <div className=" flex items-center mb-2 ">
+          {[Array(5)].map((_, i) => (
             <Star
               key={i}
-              className={`h-5 w-5 ${i < 4 ? "text-yellow-400 fill-current" : "text-gray-300"}`}
+              className={`h-5 w-5 ${
+                i < 4 ? "text-yellow-400 fill-current" : "text-gray-300"
+              }`}
             />
           ))}
+
+          <span className="text-lg font-semibold text-gray-900 ">{rating}</span>
         </div>
-        <span className="text-lg font-semibold text-gray-900">{rating}</span>
+      </div>
+      <div className="flex items-center text-gray-600 mb-3 text-xs">
+        <MapPin className="h-3 w-3 mr-1" />
+        <span>{location}</span>
       </div>
     </div>
   </div>
