eff84353486b3bdecfc557abe80ed6bbce43d53e
Riza Anas <nameisriza123boy@gmail.com>
2025-06-25 10:23:44 +0530

    Refactor HeroImage component: update border radius for improved visual consistency

--- components/groundDetails/HeroImage.tsx
+++ components/groundDetails/HeroImage.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const HeroImage = ({src,alt} : {src: string, alt: string}) => (
-  <div className="relative h-96 rounded-xl overflow-hidden shadow-lg">
+  <div className="relative h-96 rounded-b-lg overflow-hidden shadow-lg">
     <Image
       src={src}
       alt={alt}
