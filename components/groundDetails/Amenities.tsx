fa0fc9358281518ccc62cd576e24ad65924e43a9
Riza Anas <nameisriza123boy@gmail.com>
2025-06-25 11:23:41 +0530

    Refactor Amenities component: update heading size and grid layout for improved readability and consistency

--- components/groundDetails/Amenities.tsx
+++ components/groundDetails/Amenities.tsx
@@ -5,12 +5,11 @@ interface Amenity {
 
 const Amenities = ({ amenities }: { amenities: Amenity[] }) => (
   <div className="mb-6">
-    <h3 className="text-xl font-semibold text-gray-900 mb-4">Amenities</h3>
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
+    <h3 className="text-lg font-semibold text-gray-900 mb-2">Amenities</h3>
+    <div className="grid grid-cols-3 md:grid-cols-4 gap-3 md:gap-4">
       {amenities.map((amenity, index) => (
-        <div key={index} className="flex items-center space-x-3 p-3 bg-gray-50 rounded-lg">
-          <amenity.icon className="h-5 w-5 text-green-600" />
-          <span className="text-sm font-medium text-gray-700">{amenity.label}</span>
+        <div key={index} className="flex items-center px-1  bg-gray-50 rounded-2xl justify-center">
+          <span className="text-sm font-semibold text-gray-500">{amenity.label}</span>
         </div>
       ))}
     </div>
