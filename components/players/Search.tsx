f016c204c17bb349bfc333ad98ab068402d6a619
Riza Anas <nameisriza123boy@gmail.com>
2025-06-25 22:05:53 +0530

    Refactor Search component: initialize selectedDate state and improve layout for better responsiveness

--- components/players/Search.tsx
+++ components/players/Search.tsx
@@ -10,20 +10,22 @@ import LocationSelector from "../search/LocationSelector";
 import ActionButtons from "../search/ActionButtons";
 
 const Search = () => {
+  const today = new Date().toISOString().split('T')[0];
   const [selectedSport, setSelectedSport] = useState("");
-  const [selectedDate, setSelectedDate] = useState();
+  const [selectedDate, setSelectedDate] = useState(today);
   const [startTime, setStartTime] = useState("08:00 PM");
   const [endTime, setEndTime] = useState("10:00 PM");
   const [location, setLocation] = useState("Colombo");
 
+
   return (
-    <div className="mx-auto">
+    <div className= "mx-auto w-full">
       <CardContent className="space-y-8 mt-4">
-        <div className=" flex flex-col gap-4 md:flex-row">
+
+        <div className="flex flex-col gap-4 md:flex-row">
           <SportSelector
             selectedSport={selectedSport}
             setSelectedSport={setSelectedSport}
-            className=" "
           />
 
           <TimeSelector
@@ -33,15 +35,21 @@ const Search = () => {
             setEndTime={setEndTime}
           />
         </div>
-        <div className=" flex flex-col gap-4 md:flex-row">
+
+        <div className="flex flex-col gap-4 md:flex-row">
           <DateSelector
             selectedDate={selectedDate}
             setSelectedDate={setSelectedDate}
           />
 
-          <LocationSelector location={location} setLocation={setLocation} />
+          <LocationSelector
+            location={location}
+            setLocation={setLocation}
+          />
+
           <ActionButtons />
         </div>
+
       </CardContent>
     </div>
   );
