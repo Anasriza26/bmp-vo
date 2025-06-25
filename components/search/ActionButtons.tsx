9edde6958d2de024a1038171fe3422ede5749dc2
Riza Anas <nameisriza123boy@gmail.com>
2025-06-25 18:40:59 +0530

    Refactor ActionButtons component: remove Cancel button for simplified user interface

--- components/search/ActionButtons.tsx
+++ components/search/ActionButtons.tsx
@@ -8,13 +8,7 @@ export default function ActionButtons() {
         <SearchIcon className="w-5 h-5 mr-2" />
         Search
       </Button>
-      <Button
-        variant="outline"
-        className="flex-1 h-12 border-gray-300 text-gray-700 font-semibold text-lg hover:bg-gray-50"
-      >
-        <X className="w-5 h-5 mr-2" />
-        Cancel
-      </Button>
+    
     </div>
   );
 }
