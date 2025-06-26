ff3f17e1d57566cccc568b8a378d85fbdbf7cf43
Riza Anas <nameisriza123boy@gmail.com>
2025-06-26 10:45:54 +0530

    Refactor SearchButton component: improve button formatting for consistency

--- components/home/SearchButton.tsx
+++ components/home/SearchButton.tsx
@@ -6,9 +6,10 @@ import { Button } from "../ui/button";
 const SearchButton = () => (
   <div className="relative">
     <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-green-600 w-5 h-5" />
-    <Button className="w-full pl-12 pr-4 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2  bg-white text-green-600">
+    <Button className="w-full pl-12 pr-4 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2  bg-white text-green-600 ">
       Make Your play
     </Button>
+    
   </div>
 );
 
