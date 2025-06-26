c38e4839814b31234b0171486719ab55e57cf571
Riza Anas <nameisriza123boy@gmail.com>
2025-06-26 13:40:33 +0530

    Enhance search components: add hover effects to Search icon and button

--- components/bookings-page.tsx
+++ components/bookings-page.tsx
@@ -152,7 +152,7 @@ export function BookingsPage() {
         {/* Filters and Search */}
         <div className="flex flex-col sm:flex-row gap-4 mb-6 w-full">
           <div className="relative flex-1">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
+            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4 hover:bg-white" />
             <Input
               placeholder="Search booking"
               value={searchTerm}
--- components/home/SearchButton.tsx
+++ components/home/SearchButton.tsx
@@ -7,14 +7,14 @@ import SearchSheet from "./SearchSheet";
 const SearchButton = () => (
   <>
     <div className="relative md:block hidden">
-      <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-green-600 w-5 h-5" />
       <Button className="w-full pl-12 pr-4 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2  bg-white text-green-600 ">
+        <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-green-600 w-5 h-5 hover:text-white" />
         Make Your play
       </Button>
     </div>
 
     <div className="flex flex-col items-center justify-center md:hidden">
-      <Search className="h-5 w-5" />
+      <Search className="h-5 w-5 hover:bg-white" />
       <span className="text-xs mt-1"> Search</span>
     </div>
   </>
