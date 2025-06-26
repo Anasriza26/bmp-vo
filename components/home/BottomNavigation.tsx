b1e847a467eb39d7dd9f225625c459f6843069f1
Riza Anas <nameisriza123boy@gmail.com>
2025-06-26 10:44:46 +0530

    Refactor BottomNavigation component: improve structure and layout for better usability

--- components/home/BottomNavigation.tsx
+++ components/home/BottomNavigation.tsx
@@ -1,9 +1,47 @@
+"use client";
 
+import { Search, Calendar, User, Settings } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import SearchSheet from "./SearchSheet";
 
 const BottomNavigation = () => {
-  return (<>
-  </>
-  )
-}
+  return (
+    <>
+     <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t shadow-md md:hidden">
+      <div className="flex justify-around py-2">
+        {/* Search */}
 
-export default BottomNavigation
+        <Link href="/search" className="flex flex-col items-center text-center">
+          <Search className="h-5 w-5" />
+          <span className="text-xs mt-1">Search</span>
+        </Link>
+
+        {/* <Button className="flex flex-col items-center text-center">
+            <SearchSheet />
+        </Button> */}
+
+        {/* My Booking */}
+        <Link href="/check-booking" className="flex flex-col items-center text-center">
+          <Calendar className="h-5 w-5" />
+          <span className="text-xs mt-1">My Booking</span>
+        </Link>
+
+        {/* Profile */}
+        <Link href="/player-profile" className="flex flex-col items-center text-center">
+          <User className="h-5 w-5" />
+          <span className="text-xs mt-1">Profile</span>
+        </Link>
+
+        {/* Settings */}
+        <Link href="" className="flex flex-col items-center text-center">
+          <Settings className="h-5 w-5" />
+          <span className="text-xs mt-1">Settings</span>
+        </Link>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default BottomNavigation;
