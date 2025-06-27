7cf9f992a17eed4fa7bb9c658970d04d5f1c26d5
Riza Anas <nameisriza123boy@gmail.com>
2025-06-27 15:21:19 +0530

    Refactor page component: standardize formatting and improve code readability

--- app/(onboarding)/complete/page.tsx
+++ app/(onboarding)/complete/page.tsx
@@ -1,14 +1,14 @@
-
-import React from 'react'
-import Complete from '@/components/onboading/Complete'
+import React from "react";
+import Complete from "@/components/onboading/Complete";
 
 const page = () => {
-  return (<>
-  <main>
-    <Complete/>
-  </main>
-  </>
-  )
-}
+  return (
+    <>
+      <main>
+        <Complete />
+      </main>
+    </>
+  );
+};
 
-export default page
+export default page;
