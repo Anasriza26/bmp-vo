91776053178e4184a36b20e3d1dfa0d285e93623
Riza Anas <nameisriza123boy@gmail.com>
2025-06-30 15:06:21 +0530

    Refactor Complete component to replace logo with CommonLogo, improve layout, and enhance accessibility

--- components/onboading/Complete.tsx
+++ components/onboading/Complete.tsx
@@ -1,52 +1,43 @@
-import React from 'react'
-import Image from 'next/image'
-import Logo from "@/public/BMP-Logo.jpg";
+import React from "react";
+import Image from "next/image";
 import SuccessImg from "@/components/assets/Success.jpg";
-import Link from 'next/link';
+import Link from "next/link";
+import CommonLogo from "../common/CommonLogo";
 
-const complete = () => {
-  return (<>
-  <main>
-    <div className="mt-[60px] ml-[60px]">
-      <Link href={"/on-boarding"} className="flex items-center">
-      <Image src={Logo} alt="Owner Details" width={133} height={42} />
-      </Link>
-          
-        </div>
+const Complete = () => {
+  return (
+    <main className="min-h-screen flex flex-col overflow-x-hidden">
+      {/* Header */}
+      <div className="p-4">
+        <Link href="/on-boarding" className="flex items-center">
+          <CommonLogo />
+        </Link>
+      </div>
 
-        <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+      {/* Content */}
+      <div className="flex flex-1 flex-col items-center  px-4 text-center">
         <Image
-          alt="verification Img"
-          className="mt-[-120px]"
+          alt="Success"
           src={SuccessImg}
+          className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] mx-auto lg:w-[400px] lg:h-[400px]"
           width={400}
           height={400}
         />
-        <h1 className="mt-3 font-semibold text-gray-900 text-2xl leading-tight">
+        <h1 className="mt-4 font-semibold text-gray-900 text-xl md:text-2xl leading-tight">
           You're all set!
         </h1>
-        <p className="mt-1  text-gray-500 text-base leading-relaxed">
-          Congratulations! Your turf is now ready to accept bookings. Review <br /> your details or proceed to your dashboard.
+        <p className="text-gray-500 text-sm md:text-base leading-relaxed w-[300px] md:w-[600px] mt-2">
+          Congratulations! Your turf is now ready to accept bookings. Review your details or proceed to your dashboard.
         </p>
-
         <button
-  type="button"
-  className=" bg-green-600 hover:bg-green-700 text-white font-semibold text-base rounded-md px-6 py-2 mt-1"
->
-  <span className="w-5 h-5 border-2 border-white rounded-full">
-    <i className="fas fa-circle"></i>
-  </span>
-  <span className="ml-2">Access Dashboard</span>
-</button>
+          type="button"
+          className="bg-green-600 hover:bg-green-700 text-white font-semibold text-base rounded-md px-6 py-2 mt-4"
+        >
+          Access Dashboard
+        </button>
       </div>
+    </main>
+  );
+};
 
-
-    
-    
-
-  </main>
-  </>
-  )
-}
-
-export default complete
+export default Complete;
