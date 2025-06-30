3322fc988d44aefe4127ac3d05e2960d8fe43acd
Riza Anas <nameisriza123boy@gmail.com>
2025-06-30 12:04:15 +0530

    Refactor OwnerDetails component for improved layout and spacing consistency

--- components/onboading/OwnerDetails.tsx
+++ components/onboading/OwnerDetails.tsx
@@ -8,11 +8,11 @@ const ownerdetails = () => {
   return (
     <>
       <main>
-        <div className="mt-[60px] ml-[60px]">
-          <Image src={Logo} alt="Owner Details" width={133} height={42} />
+        <div className="mt-[20px] ml-[20px] lg:ml-6 md:ml-4 md:mt-[40px] ">
+          <Image src={Logo} alt="Owner Details" className="w-[60px] h-[20px] lg:w-[100px] lg:h-[32px] md:w-[80px] md:h-[22px]" />
         </div>
 
-        <div className="font-semibold">
+        <div className="font-semibold mt-2">
           <ProgressStepper
             currentStep={2}
             steps={[
@@ -25,13 +25,12 @@ const ownerdetails = () => {
           />
         </div>
 
-        <div className="justify-center items-center flex flex-col mt-[40px]">
-          <h2 className="font-semibold text-2xl font-inter text-center">
+        <div className="justify-center items-center flex flex-col md:mt-8 mt-4">
+          <h2 className="font-semibold md:text-2xl font-inter text-center text-xl">
             Tell us about yourself
           </h2>
-          <p className="font-normal text-[#667085] mt-[10px] text-center">
+          <p className="font-normal text-[#667085] mt-1 text-center md:text-[14px] text-8px ">
             Help us get to know you better. This information will be used for{" "}
-            <br />
             account creation and communication purposes.
           </p>
         </div>
