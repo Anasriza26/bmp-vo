91175487ccc6611a21d9f9fbf65732f7717bf84c
Riza Anas <nameisriza123boy@gmail.com>
2025-06-30 18:37:19 +0530

    Add rendering of AdminTransaction component in Transactions page

--- app/(admin)/admin/transactions/page.tsx
+++ app/(admin)/admin/transactions/page.tsx
@@ -1,7 +1,9 @@
+import AdminTransaction from "@/components/admin/AdminTransaction";
 import React from "react";
 
 const Transactions = () => {
-  return <></>;
+  return <>
+  <AdminTransaction /></>;
 };
 
 export default Transactions;
