9a3276dbb1078fbe1528cf3025bd87f097907044
Riza Anas <nameisriza123boy@gmail.com>
2025-06-30 12:08:59 +0530

    Replace logo image with CommonLogo component for consistency

--- components/onboading/OwnerDetails.tsx
+++ components/onboading/OwnerDetails.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Image from "next/image";
-import Logo from "../assets/BMP-Logo.jpg";
 import ProgressStepper from "../progress-stepper/Progress";
 import Register from "../onboading/onboarding-helper/Register";
+import CommonLogo from "../common/CommonLogo";
 
 const ownerdetails = () => {
   return (
     <>
       <main>
-        <div className="mt-[20px] ml-[20px] lg:ml-6 md:ml-4 md:mt-[40px] ">
-          <Image src={Logo} alt="Owner Details" className="w-[60px] h-[20px] lg:w-[100px] lg:h-[32px] md:w-[80px] md:h-[22px]" />
-        </div>
+        <CommonLogo />
 
         <div className="font-semibold mt-2">
           <ProgressStepper
