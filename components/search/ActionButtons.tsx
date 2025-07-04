2eb7019742165fc41819221dd3c9e7dc2024990d
Riza Anas <nameisriza123boy@gmail.com>
2025-07-04 16:55:42 +0530

    Add button alignment

--- components/search/ActionButtons.tsx
+++ components/search/ActionButtons.tsx
@@ -3,10 +3,14 @@ import { Search as SearchIcon, X } from "lucide-react";
 
 export default function ActionButtons() {
   return (
-    <div className="flex flex-col sm:flex-row md:pt-14">
-      <Button className="flex-1 h-12 bg-green-500 hover:bg-green-600 text-white font-semibold text-lg ">
+    <div className="grid md:grid-cols-2 gap-2 mt-4">
+      <Button className="flex-1bg-green-500 hover:bg-green-600 text-white font-semibold text-lg ">
         <SearchIcon className="w-5 h-5 mr-2" />
         Search
+      </Button>
+
+      <Button className="text-[#344054] font-semibold text-lg bg-white border-2 border-[#344054]">
+        Cancel
       </Button>
     
     </div>
