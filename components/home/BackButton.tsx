26066990c516bf2c6020b328a09d706ab0da92ff
Riza Anas <nameisriza123boy@gmail.com>
2025-07-05 13:55:47 +0530

    create a component back button component

---
+++ components/home/BackButton.tsx
@@ -0,0 +1,16 @@
+import { ArrowLeft, Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export const BackButton = () => {
+  return (
+    <>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="h-10 w-10 rounded-full bg-gray-100"
+      >
+        <ArrowLeft className="h-5 w-5" />
+      </Button>
+    </>
+  );
+};
