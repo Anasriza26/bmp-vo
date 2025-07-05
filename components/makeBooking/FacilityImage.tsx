a0fece67d0e133d5ef9a17de002dac5d251220c0
Riza Anas <nameisriza123boy@gmail.com>
2025-07-05 11:24:13 +0530

    remove shadow effect

--- components/makeBooking/FacilityImage.tsx
+++ components/makeBooking/FacilityImage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const FacilityImage: React.FC = () => {
   return (
-    <div className="relative w-full h-64 sm:h-96 rounded-b-lg overflow-hidden shadow-sm">
+    <div className="relative w-full h-64 sm:h-96 rounded-b-lg overflow-hidden mb-2">
       <Image
         src="https://images.pexels.com/photos/209977/pexels-photo-209977.jpeg?auto=compress&cs=tinysrgb&w=1200&h=600&fit=crop"
         alt="Futsal Prime - FD aerial view"
